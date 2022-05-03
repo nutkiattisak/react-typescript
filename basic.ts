fdc95e55f4dc47061356864c739b702d14ffa0e1
Kiattisak Chantharamaneechote <kiattisak.ch1995@gmail.com>
2022-05-03 23:57:58 +0700

    initial commit

---
+++ basic.ts
@@ -0,0 +1,37 @@
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "Nut Kiattisak";
+
+let isShow: boolean;
+
+isShow = true;
+let programming: string[];
+
+programming = ["JavaScript", "TypeScript", "Java", "C#"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Kiattisak",
+  age: 27,
+};
+
+// person = {
+//   isInactive: true,
+// };
+
+let people: {
+  name: string;
+  age: number;
+}[];
+
+let course: string = "React";
+
+course = 12345;
